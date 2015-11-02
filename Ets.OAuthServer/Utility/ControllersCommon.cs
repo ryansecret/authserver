829789f56592e6a8b985587fbd3cba8426615e13
linyanping <lin.yanping@etaostars.com>
2015-11-02 10:41:43 +0800

    pro：手机验证码登录

---
+++ Ets.OAuthServer/Utility/ControllersCommon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ets.OAuthServer.Utility
+{
+    public static class ControllersCommon
+    {
+        /// <summary>
+        /// 生成验证码临时用户Id
+        /// </summary>
+        /// 创建者：林燕平
+        /// 创建日期：11/2/2015 10:21 AM
+        /// 修改者：
+        /// 修改时间：
+        /// ----------------------------------------------------------------------------------------
+        public const string ConstSessionUserIdForCode = "UserIdForCode";
+    }
+}
